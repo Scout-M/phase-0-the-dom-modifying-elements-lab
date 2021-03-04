4dabd6192390f7ce0c9a1212623ef4d1c34ce3bf
Amy Mangor <aemangor@gmail.com>
2021-03-04 16:33:42 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+let main = document.querySelector('main');
+main.remove();
+
+
+let newHeader = document.createElement('h1');
+newHeader.id= 'victory';
+newHeader.innerHTML = '<h1> Mangor is the champion </h1>';
+document.body.appendChild(newHeader);
+
+
